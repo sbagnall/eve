816c0244f5a05c9e8114d09e9fd3fe6cb718f6a1
sbagnall <steve.bagnall@gmail.com>
2015-10-24 19:17:06 +0100

    update

--- market.js
+++ market.js
@@ -1,3 +1,22 @@
 module.exports = function () {
 	'use strict';
-}
+
+	var options = {
+		host: 'api.eve-central.com',
+		port: 80,
+		path: '/api/marketstat/json?typeid=34',
+		method: 'GET'
+	},
+	http = require('http');
+
+
+	http.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	}).end();
+
+};
