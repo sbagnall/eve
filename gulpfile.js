d7b1f0de530c605e8051d4b1eb8487da01215724
Steve Bagnall <steve.bagnall@gmail.com>
2015-10-28 13:25:59 +0000

    exclude new folders from jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint =require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
 
 gulp.task('jshint', function () {
-	return gulp.src(['./**/*.js', '!./node_modules/**/*.*'])
+	return gulp.src(['./**/*.js', '!./node_modules/**/*.*', '!./eveoj/**/*.*', '!./sdd2json/**/*.*'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
