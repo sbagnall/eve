0c1e6f5c4a956d6768c9dee086bfd778a77856f5
sbagnall <steve.bagnall@gmail.com>
2016-01-07 08:33:56 +0000

    added jshint ignore

--- staticData.js
+++ staticData.js
@@ -7,27 +7,43 @@ module.exports = (function() {
 		sqlite3 = require('sqlite3').verbose(),
 		db = new sqlite3.Database(dbFile);
 
+	function foundItem (err, row, cb) {
+		if (err) {
+			cbItem(err);
+		} else {
+			cb(null, { 
+				id: row.regionID,
+				name: row.regionName
+			});
+		}
+	}
+
+	fkunction competeItem (err, row, cb) {
+		if (err) {
+			cbComplete(err);
+		} else {
+			cbComplete(null, found);
+		}
+	}
+
+	function mapRegion (regionID, cbItem, cbComplete) {
+		if (isDbFileExists) {
+			db.get(
+				'SELECT * FROM mapRegions WHERE regionID = ''' + regionID + '''',
+				founditem(err, row, cbItem),
+				completeitem(err, row, cbItem));
+		}
+	}
+
 	function mapRegions (cbItem, cbComplete) {
 
 		if (isDbFileExists) {
 			db.each('SELECT * FROM mapRegions', 
 				function item (err, row) {
-					if (err) {
-						cbItem(err);
-					} else {
-						cbItem(null, { 
-								id: row.regionID,
-								name: row.regionName
-							});
-					}
-					
+					foundItem(err, row, cbitem)
 				},
 				function complete (err, found) {
-					if (err) {
-						cbComplete(err);
-					} else {
-						cbComplete(null, found);
-					}
+					competeItem(err, row, cbComplete)
 				});
 		}
 	}
